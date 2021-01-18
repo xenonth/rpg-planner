5920f57fa9df37d7db24cefd2584d93fe3c69cc8
Timothy Forbes <timothy.forbes259@gmail.com>
2021-01-18 14:14:55 +1030

    database ready to seed code not recognising settlement schema file, require assistance from supervisor on wise course of action.

---
+++ models/settlementIndex.js
@@ -0,0 +1,3 @@
+module.exports = {
+    Settlement: require("./settlementSchema")
+  };
--- models/settlementSchema.js
+++ models/settlementSchema.js
@@ -5,15 +5,31 @@ const Schema = mongoose.Schema;
 const settlementSchema = new Schema ({
     name: {type: String, required: true},
     type: {type: String, required: true},
+    size: {type: Number, required: true},
     government: {type: String, required: true},
-    description: {type: String, required: true}, 
+    description: {type: String, required: true},
+    
     population: {
-        number: {type: String, required: true},
-        races: [{
-            raceName: {type: Array, required: true},
-            percentage: {type: Number, required: true}
-        }],
+        numberOfRaces: {type: Number, required: true},
+        raceOne: {
+            raceName: {type: String, required: true},
+            percentage: {type: Number, required: true},
+        },
+        raceTwo: {
+            raceName: String,
+            percentage: Number,
+        },
+        raceThree: {
+            raceName: String,
+            percentage: Number,
+        },
+        raceFour: {
+            raceName: String,
+            percentage: Number,
+        }
     }, 
+    otherInformation: String,
+    date: { type: Date, default: Date.now },
 })
 
 const Settlement = mongoose.model("Settlement", settlementSchema);
