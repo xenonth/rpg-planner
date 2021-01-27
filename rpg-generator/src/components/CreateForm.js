e813ffca8e8731b20f3e73d59a177be41ef57366
Timothy Forbes <timothy.forbes259@gmail.com>
2021-01-27 18:42:07 +1030

    successfully rendered input field data to the console

--- rpg-generator/src/components/CreateForm.js
+++ rpg-generator/src/components/CreateForm.js
@@ -31,35 +31,26 @@ class CreateForm extends Component {
   
   handleChange(event) {
     this.setState({
-      townValue: event.target.value,
-      governValue: event.target.value,
-      typeValue: event.target.value,
-      populationValue: event.target.value,
-      descriptionValue: event.target.value,
-      raceOneValue: event.target.value,
-      raceTwoValue: event.target.value,
-      raceThreeValue: event.target.value,
-      raceFourValue: event.target.value,
-      otherValue: event.target.value,
+      [event.target.name]: event.target.value,
     
     });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    alert(
-      `Your town name is: ${this.state.townValue}`
-      `Your govern Style is: ${this.state.governValue}`
-      `Your type is: ${this.state.typeValue}`
-      `Your town is like: ${this.state.descriptionValue}`
-      `A majority race which lives here is: ${this.state.raceOneValue}`
-      `A majority race which lives here is: ${this.state.raceTwoValue}`
-      `A majority race which lives here is: ${this.state.raceThreeValue}`
-      `A majority race which lives here is: ${this.state.raceFourValue}`
-      `Things happening in Town include: ${this.state.otherValue}`
+    
+      console.log(this.state.townValue);
+      console.log(this.state.governValue);
+      console.log(this.state.typeValue);
+      console.log(this.state.descriptionValue);
+      console.log(this.state.raceOneValue);
+      console.log(this.state.raceTwoValue);
+      console.log(this.state.raceThreeValue);
+     console.log(this.state.raceFourValue);
+      console.log(this.state.otherValue);
       //Code to check that the population field is a number or integer
         //if this.state.popvalue !== Number convert number or alert to reenter a value
-    );
+    
   }
 
   //code for an API function to send a post request to the API in the object layout
@@ -73,17 +64,17 @@ class CreateForm extends Component {
           
             <Form.Group>
               <Form.Label>Town Name</Form.Label>
-              <Form.Control as='input' placeholder="Settlement Name" value={this.state.townValue} onChange={this.handleChange} />
+              <Form.Control as='input' name='townValue' placeholder="Settlement Name" value={this.state.townValue} onChange={this.handleChange} />
             </Form.Group>
 
             <Form.Group>
               <Form.Label>Government</Form.Label>
-              <Form.Control as='input' placeholder="How is Your Town Ruled" value={this.state.governValue} onChange={this.handleChange} />
+              <Form.Control as='input' name='governValue' placeholder="How is Your Town Ruled" value={this.state.governValue} onChange={this.handleChange} />
             </Form.Group>
 
             <Form.Group>
               <Form.Label>Type</Form.Label>
-              <Form.Control as="input" placeholder="What size category is your settlement?" value={this.state.governValue} onChange={this.handleChange} />
+              <Form.Control as="input" name='typeValue' placeholder="What size category is your settlement?" value={this.state.typeValue} onChange={this.handleChange} />
               <Form.Text className="text-muted">
                 Is it a hamlet, farming-community, village, town, city, monastic order?.
               </Form.Text>
@@ -91,7 +82,7 @@ class CreateForm extends Component {
 
             <Form.Group>
               <Form.Label>Population</Form.Label>
-              <Form.Control as="input" placeholder="How many people live in your settlement?" value={this.state.governValue} onChange={this.handleChange} />
+              <Form.Control as="input" name='populationValue' placeholder="How many people live in your settlement?" value={this.state.populationValue} onChange={this.handleChange} />
               <Form.Text className="text-muted">
                 Please fill in hte above as an integer.
               </Form.Text>
@@ -99,7 +90,7 @@ class CreateForm extends Component {
 
             <Form.Group>
               <Form.Label>Description:</Form.Label>
-              <Form.Control as="textarea" rows={5} type="text" value={this.state.descriptionValue} onChange={this.handleChange}/>
+              <Form.Control as="textarea" rows={5} name='descriptionValue' type="text" value={this.state.descriptionValue} onChange={this.handleChange}/>
               <Form.Text className="text-muted">
                 What does your settlement look like?
             </Form.Text>
@@ -108,28 +99,53 @@ class CreateForm extends Component {
             <Form.Row>
               <Form.Group as={Col} controlId="formRaceOne" >
               <Form.Label>Race Name:</Form.Label>
-              <Form.Control as='input' placeholder="Race Name" value={this.state.raceOneValue} onChange={this.handleChange}/>
+              <Form.Control as='input' name='raceOneValue' placeholder="Race Name" value={this.state.raceOneValue} onChange={this.handleChange}/>
               </Form.Group>
 
               <Form.Group as={Col} controlId="formRaceTwo" >
               <Form.Label>Race Name:</Form.Label>
-              <Form.Control as='input' placeholder="Race Name" value={this.state.raceTwoValue} onChange={this.handleChange}/>
+              <Form.Control as='input' placeholder="Race Name" name='raceTwoValue' value={this.state.raceTwoValue} onChange={this.handleChange}/>
               </Form.Group>
 
               <Form.Group as={Col} controlId="formRaceThree" >
                 <Form.Label>Race Name:</Form.Label>
-                <Form.Control as='input' placeholder="Race Name" value={this.state.raceThreeValue} onChange={this.handleChange} />
+                <Form.Control as='input' placeholder="Race Name" name='raceThreeValue' value={this.state.raceThreeValue} onChange={this.handleChange} />
               </Form.Group>
 
               <Form.Group as={Col} controlId="formRaceFour" >
                 <Form.Label>Race Name:</Form.Label>
-                <Form.Control as='input' placeholder="Race Name" value={this.state.raceFourValue} onChange={this.handleChange} />
+                <Form.Control as='input' placeholder="Race Name" name='raceFourValue' value={this.state.raceFourValue} onChange={this.handleChange} />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+            <Form.Text className="text-muted">
+              Key NPCS, events, what is that shifty blind man up to?
+            </Form.Text>
+              <Form.Group as={Col} controlId="formRaceOne" >
+              <Form.Label>percentage:</Form.Label>
+              <Form.Control as='input' name='raceOneValue' placeholder="Race Name" value={this.state.raceOneValue} onChange={this.handleChange}/>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formRaceTwo" >
+              <Form.Label>Race Name:</Form.Label>
+              <Form.Control as='input' placeholder="Race Name" name='raceTwoValue' value={this.state.raceTwoValue} onChange={this.handleChange}/>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formRaceThree" >
+                <Form.Label>Race Name:</Form.Label>
+                <Form.Control as='input' placeholder="Race Name" name='raceThreeValue' value={this.state.raceThreeValue} onChange={this.handleChange} />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formRaceFour" >
+                <Form.Label>Race Name:</Form.Label>
+                <Form.Control as='input' placeholder="Race Name" name='raceFourValue' value={this.state.raceFourValue} onChange={this.handleChange} />
               </Form.Group>
             </Form.Row>
 
             <Form.Group>
               <Form.Label>Other Information:</Form.Label>
-              <Form.Control as="textarea" rows={5} type="text" value={this.state.otherValue} onChange={this.handleChange} />
+              <Form.Control as="textarea" rows={5} type="text" name='otherValue' value={this.state.otherValue} onChange={this.handleChange} />
               <Form.Text className="text-muted">
               Key NPCS, events, what is that shifty blind man up to?
             </Form.Text>
